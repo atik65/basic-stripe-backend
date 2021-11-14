6bfe05d208ca2d16927e70f60f1d76037ff8db3c
Atik Hasan <atikhasan801@gmail.com>
2021-11-14 22:10:19 +0600

    stripe backend with exprss

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.stripeKey);
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// port
+const port = process.env.PORT || 5000;
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Stripe BackEnd");
+});
+
+app.post("/payment", (req, res) => {
+  const { token, product } = req.body;
+
+  const idempotencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((error) => console.log(error));
+});
+
+// listen part
+
+app.listen(port, () => {
+  console.log("Listening to port = ", port);
+});
